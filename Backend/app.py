02884521b0ae5e23a541ed1d231e66d50a0d5f66
polinaKa-hub <polinaka2020@gmail.com>
2025-12-29 12:49:56 +0300

    Prepare backend for deployment

--- Backend/app.py
+++ Backend/app.py
@@ -422,4 +422,4 @@ def serve_avatar(filename):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0')
--- Backend/config.py
+++ Backend/config.py
@@ -5,7 +5,11 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///cookbook.db')  
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///cookbook.db')  
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+    'DATABASE_URL',
+    'sqlite:///instance/cookbook.db'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file size
